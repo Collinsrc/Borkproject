df2e2d1528960063a139adb7b38ac70a8eb33935
Awesomeman360 <47046011+Awesomeman360@users.noreply.github.com>
2019-02-24 19:39:01 -0500

    Changes to fit with game style
    Added the "isWeapon" attribute to the Item Class so that we can create weapons for the players to use during combat.
    Add the "Damage" attribute to items as well to determine how much damage the weapons will do when combat is implemented
    - Added minimum and maximum damage so a random number can be generated within the damage range
    
    Altered the game to make sense in a dungeon related context (rooms and Items).

--- Item.java
+++ Item.java
@@ -1,23 +1,32 @@
 
 /**
  * Write a description of class Item here.
- * 
- * @author Kevin Smith  
- * @version 4/12/2017
+ *
+ * @author Kevin Smith, Haris Islamcevic,
+ * @version 02/24/2019
  */
 public class Item
 {
     /** Instance variable for name */
     String name;
-    
+
     /** Instance variable for description */
     String description;
-    
+
     /** Instance variable for weight */
     int weight;
-    
+
     /** Instance variable for edible */
-    boolean edible; 
+    boolean edible;
+
+    /** Instance variable for weapon */
+    boolean isWeapon;
+
+    /** Instance variable for minimum damage the weapon can do */
+    int minimumDamage;
+
+    /** Instance variable for maximum damage the weapon can do */
+    int maximumDamage;
 
     /**
      * Constructor for objects of class Item
@@ -28,17 +37,22 @@ public class Item
         description = "";
         weight = 0;
         edible = false;
+        isWeapon = false;
+        minimumDamage = 0;
+        maximumDamage = 0;
     }
 
     /**
      * Constructor for objects of class Item
      * @param n,d,w,e
      */
-    public Item(String n, String d, int w, boolean e){
+    public Item(String n, String d, int w, boolean e, boolean wpn, int minD, int maxD){
         name = n;
         description = d;
         weight = w;
         edible = e;
+        isWeapon = wpn;
+        damage = dmg;
     }
 
     /**
@@ -90,7 +104,7 @@ public class Item
     }
 
     /**
-     * Method to set if the item is edible or not. 
+     * Method to set if the item is edible or not.
      * @param edible
      */
     public void setEdible(boolean edible) {
@@ -98,12 +112,50 @@ public class Item
     }
 
     /**
-     * Method that sets edible to true. 
+     * Method that sets edible to true.
      * return edible
      */
     public boolean isEdible(){
         return edible = true;
     }
 
-}
+    /**
+     * Method that sets isWeapon to true.
+     * return edible
+     */
+    public boolean isWeapon(){
+        return isWeapon = true;
+    }
 
+    /**
+     * Method to get a weapon's minimum damage.
+     * return weight
+     */
+     public int getMinimumDamage() {
+         return minimumDamage;
+     }
+
+     /**
+      * Method to get a weapon's maxiumum damage.
+      * return weight
+      */
+     public int getMaximumDamage() {
+         return maximumDamage;
+     }
+
+     /**
+      * Method to set a weapon's minimum damage.
+      * @param minimumDamage
+      */
+     public void setWeight(int damage) {
+         this.minimumDamage = damage;
+     }
+
+     /**
+      * Method to set a weapon's maximum damage.
+      * @param maximumDamage
+      */
+     public void setWeight(int damage) {
+         this.maximumDamage = damage;
+     }
+}

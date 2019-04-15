46fb48f514ce621137f74a1b5d9fd5f089f0dfaa
Robert Collins <collirob@mail.gvsu.edu>
2019-04-15 19:42:34 -0400

    Tons of checkstyle error fixes 1300 errors to 100

--- GameGUI.java
+++ GameGUI.java
@@ -1,353 +1,338 @@
 package BORK;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
-/*************************************************************
- * GUI for a a text based game
+/**
+ * *********************************************************** GUI for a a text based game.
  *
  * @author Kevin Smith, Haris Islamcevic, Robert Collins
- * @version 02/24/2019
- ************************************************************/
-public class GameGUI extends JFrame implements ActionListener{
+ * @version 02/24/2019 **********************************************************
+ */
+public class GameGUI extends JFrame implements ActionListener {
 
-	/** Instance variable that declares Game */
-	Game g;
+  /** Instance variable that declares Game */
+  Game g;
 
-	/** JButtons */
-	private JButton east;
-	private JButton west;
-	private JButton north;
-	private JButton south;
-	private JButton help;
-	private JButton pickup;
-	private JButton drop;
-	private JButton eat;
-	private JButton look;
-	private JButton list;
-	private JButton slide;
-	private JButton toss;
-	private JButton showMinimap;
-	
-	/** Displays results in this text area */
-	JTextArea results;
+  /** JButtons */
+  private JButton east;
 
-	/** menu items */
-	JMenuBar menus;
-	JMenu fileMenu;
-	JMenuItem quitItem;
+  private JButton west;
+  private JButton north;
+  private JButton south;
+  private JButton help;
+  private JButton pickup;
+  private JButton drop;
+  private JButton eat;
+  private JButton look;
+  private JButton list;
+  private JButton slide;
+  private JButton toss;
+  private JButton showMinimap;
 
-	/*****************************************************************
-	 * Main Method
-	 ****************************************************************/
-	public static void main(String args[]){
-		GameGUI gui = new GameGUI();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setTitle("BORK");
-		gui.setSize(200,200);
-		gui.pack();
-		gui.setVisible(true);
+  /** Displays results in this text area */
+  JTextArea results;
 
-	}
+  /** menu items */
+  JMenuBar menus;
 
-	/*****************************************************************
-	 * constructor installs all of the GUI components
-	 ****************************************************************/
-	public GameGUI(){
-		g = new Game();
+  JMenu fileMenu;
+  JMenuItem quitItem;
 
-		setLayout(new GridBagLayout());
-		GridBagConstraints loc;
-		
-		results = new JTextArea(20,50);
-		JScrollPane scrollPane = new JScrollPane(results);
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 0;
-		loc.insets.left = 5;
-		loc.insets.bottom = 5;
-		add(scrollPane, loc);
+  /**
+   * *************************************************************** Main Method
+   * **************************************************************
+   */
+  public static void main(String args[]) {
+    GameGUI gui = new GameGUI();
+    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    gui.setTitle("BORK");
+    gui.setSize(200, 200);
+    gui.pack();
+    gui.setVisible(true);
+  }
 
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,500);
-		add(new JLabel("Actions: "), loc);
+  /**
+   * *************************************************************** constructor installs all of the
+   * GUI components **************************************************************
+   */
+  public GameGUI() {
+    g = new Game();
 
-		loc = new GridBagConstraints();
-		loc.gridx = 1;
-		loc.gridy = 0;
-		loc.insets =  new Insets(0,50,150,0);
-		add(new JLabel("Directions: "), loc);
+    setLayout(new GridBagLayout());
+    GridBagConstraints loc;
 
-		look = new JButton();
-		look.setText("Look");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,350);
-		add(look, loc);
+    results = new JTextArea(20, 50);
+    JScrollPane scrollPane = new JScrollPane(results);
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 0;
+    loc.insets.left = 5;
+    loc.insets.bottom = 5;
+    add(scrollPane, loc);
 
-		pickup = new JButton();
-		pickup.setText("Pick up");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,200);
-		add(pickup, loc);
-		drop = new JButton();
-		drop.setText("Drop");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,50);
-		add(drop, loc);
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, 500);
+    add(new JLabel("Actions: "), loc);
 
-		eat = new JButton();
-		eat.setText("Eat");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,-75);
-		add(eat, loc);
+    loc = new GridBagConstraints();
+    loc.gridx = 1;
+    loc.gridy = 0;
+    loc.insets = new Insets(0, 50, 150, 0);
+    add(new JLabel("Directions: "), loc);
 
-		help = new JButton();
-		help.setText("help");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,-200);
-		add(help, loc);
+    look = new JButton();
+    look.setText("Look");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, 350);
+    add(look, loc);
 
-		list = new JButton();
-		list.setText("List");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,-325);
-		add(list, loc);
-		slide = new JButton();
-		slide.setText("Swipe");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,-465);
-		add(slide, loc);
+    pickup = new JButton();
+    pickup.setText("Pick up");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, 200);
+    add(pickup, loc);
+    drop = new JButton();
+    drop.setText("Drop");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, 50);
+    add(drop, loc);
 
-		toss = new JButton();
-		toss.setText("Throw");
-		loc = new GridBagConstraints();
-		loc.gridx = 0;
-		loc.gridy = 1;
-		loc.insets =  new Insets(0,0,0,-615);
-		add(toss, loc);
+    eat = new JButton();
+    eat.setText("Eat");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, -75);
+    add(eat, loc);
 
-		north = new JButton();
-		north.setText("North");
-		loc = new GridBagConstraints();
-		loc.gridx = 1;
-		loc.gridy = 0;
-		loc.insets =  new Insets(0,50,100,0);
-		add(north, loc);
+    help = new JButton();
+    help.setText("help");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, -200);
+    add(help, loc);
 
-		west = new JButton();
-		west.setText("West");
-		loc = new GridBagConstraints();
-		loc.gridx = 1;
-		loc.gridy = 0;
-		loc.insets =  new Insets(0,50,25,0);
-		add(west, loc);
-		south = new JButton();
-		south.setText("South");
-		loc = new GridBagConstraints();
-		loc.gridx = 1;
-		loc.gridy = 0;
-		loc.insets =  new Insets(0,50,-50,0);
-		add(south, loc);
+    list = new JButton();
+    list.setText("List");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, -325);
+    add(list, loc);
+    slide = new JButton();
+    slide.setText("Swipe");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, -465);
+    add(slide, loc);
 
-		east = new JButton();
-		east.setText("East");
-		loc = new GridBagConstraints();
-		loc.gridx = 1;
-		loc.gridy = 0;
-		loc.insets =  new Insets(0,50,-125,0);
-		add(east, loc);
-		
-		showMinimap = new JButton();
-		showMinimap.setText("Minimap");
-		loc = new GridBagConstraints();
-		loc.gridx = 1;
-		loc.gridy = 0;
-		loc.insets = new Insets (0, 50, -200, 0);
-		add(showMinimap, loc);
+    toss = new JButton();
+    toss.setText("Throw");
+    loc = new GridBagConstraints();
+    loc.gridx = 0;
+    loc.gridy = 1;
+    loc.insets = new Insets(0, 0, 0, -615);
+    add(toss, loc);
 
-		// FIX ME: register the button action listeners
-		north.addActionListener(this);
-		south.addActionListener(this);
-		east.addActionListener(this);
-		west.addActionListener(this);
-		help.addActionListener(this);
-		pickup.addActionListener(this);
-		drop.addActionListener(this);
-		eat.addActionListener(this);
-		look.addActionListener(this);
-		list.addActionListener(this);
-		slide.addActionListener(this);
-		toss.addActionListener(this);
-		showMinimap.addActionListener(this);
+    north = new JButton();
+    north.setText("North");
+    loc = new GridBagConstraints();
+    loc.gridx = 1;
+    loc.gridy = 0;
+    loc.insets = new Insets(0, 50, 100, 0);
+    add(north, loc);
 
-		fileMenu = new JMenu("File");
-		quitItem = new JMenuItem("Quit");
-		fileMenu.add(quitItem);
-		menus = new JMenuBar();
-		setJMenuBar(menus);
-		menus.add(fileMenu);
+    west = new JButton();
+    west.setText("West");
+    loc = new GridBagConstraints();
+    loc.gridx = 1;
+    loc.gridy = 0;
+    loc.insets = new Insets(0, 50, 25, 0);
+    add(west, loc);
+    south = new JButton();
+    south.setText("South");
+    loc = new GridBagConstraints();
+    loc.gridx = 1;
+    loc.gridy = 0;
+    loc.insets = new Insets(0, 50, -50, 0);
+    add(south, loc);
 
-		fileMenu.addActionListener(this);
-		quitItem.addActionListener(this);
+    east = new JButton();
+    east.setText("East");
+    loc = new GridBagConstraints();
+    loc.gridx = 1;
+    loc.gridy = 0;
+    loc.insets = new Insets(0, 50, -125, 0);
+    add(east, loc);
 
-		g.setWelcomeMessage();
-		results.append(g.getMessage());
+    showMinimap = new JButton();
+    showMinimap.setText("Minimap");
+    loc = new GridBagConstraints();
+    loc.gridx = 1;
+    loc.gridy = 0;
+    loc.insets = new Insets(0, 50, -200, 0);
+    add(showMinimap, loc);
 
-	}
+    // FIX ME: register the button action listeners
+    north.addActionListener(this);
+    south.addActionListener(this);
+    east.addActionListener(this);
+    west.addActionListener(this);
+    help.addActionListener(this);
+    pickup.addActionListener(this);
+    drop.addActionListener(this);
+    eat.addActionListener(this);
+    look.addActionListener(this);
+    list.addActionListener(this);
+    slide.addActionListener(this);
+    toss.addActionListener(this);
+    showMinimap.addActionListener(this);
 
-	/*****************************************************************
-	 * This method is called when any button is clicked.  The proper
-	 * internal method is called as needed.
-	 *
-	 * @param e the event that was fired
-	 ****************************************************************/
-	public void actionPerformed(ActionEvent e){		
-		// menu item - quit
-		if (e.getSource() == quitItem){
-			System.exit(1);
-		}
-		else if (e.getSource() == look){
-			g.look();
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == pickup){
-			g.pickup();
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == drop){
-			String toDrop = JOptionPane.showInputDialog(null, "Drop what?");
-			g.drop(toDrop);
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == eat){
-			String toEat = JOptionPane.showInputDialog(null, "Eat what?");
-			g.eat(toEat);
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == list){
-			g.list();
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == north){
-			g.move("north");
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == east){
-			g.move("east");
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == west){
-			g.move("west");
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == south){
-			g.move("south");
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == toss){
-			String toToss = JOptionPane.showInputDialog(null, "Throw what?");
-			g.toss(toToss);
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if (e.getSource() == slide){
-			String toSlide = JOptionPane.showInputDialog(null, "Swipe what?");
-			g.slide(toSlide);
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if(e.getSource() == showMinimap) {
-			JFrame minimapDisplay = new JFrame();
-			minimapDisplay.setLayout(new FlowLayout());
-			minimapDisplay.setSize(575, 375);
-			minimapDisplay.setResizable(false);
-			minimapDisplay.setVisible(true);
-			minimapDisplay.setTitle("BORK MiniMap");
-			
-			ImageIcon image = new ImageIcon("minimap.jpg");
-			JLabel imageLabel = new JLabel(image);
-			imageLabel.setBounds(10, 10, 575, 375);
-			imageLabel.setVisible(true);
-			minimapDisplay.add(imageLabel);
-		}
-		else if (e.getSource() == help){
-			g.help();
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-		else if(g.gameOver() == true){
-			gameOver();
-			String msg = g.getMessage();
-			results.append("\n" + msg);
-		}
-	}
+    fileMenu = new JMenu("File");
+    quitItem = new JMenuItem("Quit");
+    fileMenu.add(quitItem);
+    menus = new JMenuBar();
+    setJMenuBar(menus);
+    menus.add(fileMenu);
 
-	/**
-	 * Method that disables all the buttons when the game is over
-	 */
-	private void gameOver(){
-		if(g.gameOver() == true){
-			look.setEnabled(false);
-			pickup.setEnabled(false);
-			drop.setEnabled(false);
-			eat.setEnabled(false);
-			list.setEnabled(false);
-			help.setEnabled(false);
-			slide.setEnabled(false);
-			toss.setEnabled(false);
-			north.setEnabled(false);
-			east.setEnabled(false);
-			west.setEnabled(false);
-			south.setEnabled(false);
-			showMinimap.setEnabled(false);
-		}
-	}
+    fileMenu.addActionListener(this);
+    quitItem.addActionListener(this);
 
-	/**
-	 * Method that creates a new game for the player
-	 */
-	private void newGame(){
-		g = new Game();
-		look.setEnabled(true);
-		pickup.setEnabled(true);
-		drop.setEnabled(true);
-		eat.setEnabled(true);
-		list.setEnabled(true);
-		help.setEnabled(true);
-		slide.setEnabled(true);
-		toss.setEnabled(true);
-		north.setEnabled(true);
-		east.setEnabled(true);
-		west.setEnabled(true);
-		south.setEnabled(true);
-		showMinimap.setEnabled(true);
-		g.setWelcomeMessage();
-		results.setText("");
-	}
-	
+    g.setWelcomeMessage();
+    results.append(g.getMessage());
+  }
+
+  /**
+   * *************************************************************** This method is called when any
+   * button is clicked. The proper internal method is called as needed.
+   *
+   * @param e the event that was fired
+   *     **************************************************************
+   */
+  public void actionPerformed(ActionEvent e) {
+    // menu item - quit
+    if (e.getSource() == quitItem) {
+      System.exit(1);
+    } else if (e.getSource() == look) {
+      g.look();
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == pickup) {
+      g.pickup();
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == drop) {
+      String toDrop = JOptionPane.showInputDialog(null, "Drop what?");
+      g.drop(toDrop);
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == eat) {
+      String toEat = JOptionPane.showInputDialog(null, "Eat what?");
+      g.eat(toEat);
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == list) {
+      g.list();
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == north) {
+      g.move("north");
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == east) {
+      g.move("east");
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == west) {
+      g.move("west");
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == south) {
+      g.move("south");
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == toss) {
+      String toToss = JOptionPane.showInputDialog(null, "Throw what?");
+      g.toss(toToss);
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == slide) {
+      String toSlide = JOptionPane.showInputDialog(null, "Swipe what?");
+      g.slide(toSlide);
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (e.getSource() == showMinimap) {
+      JFrame minimapDisplay = new JFrame();
+      minimapDisplay.setLayout(new FlowLayout());
+      minimapDisplay.setSize(575, 375);
+      minimapDisplay.setResizable(false);
+      minimapDisplay.setVisible(true);
+      minimapDisplay.setTitle("BORK MiniMap");
+
+      ImageIcon image = new ImageIcon("minimap.jpg");
+      JLabel imageLabel = new JLabel(image);
+      imageLabel.setBounds(10, 10, 575, 375);
+      imageLabel.setVisible(true);
+      minimapDisplay.add(imageLabel);
+    } else if (e.getSource() == help) {
+      g.help();
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    } else if (g.gameOver() == true) {
+      gameOver();
+      String msg = g.getMessage();
+      results.append("\n" + msg);
+    }
+  }
+
+  /** Method that disables all the buttons when the game is over */
+  private void gameOver() {
+    if (g.gameOver() == true) {
+      look.setEnabled(false);
+      pickup.setEnabled(false);
+      drop.setEnabled(false);
+      eat.setEnabled(false);
+      list.setEnabled(false);
+      help.setEnabled(false);
+      slide.setEnabled(false);
+      toss.setEnabled(false);
+      north.setEnabled(false);
+      east.setEnabled(false);
+      west.setEnabled(false);
+      south.setEnabled(false);
+      showMinimap.setEnabled(false);
+    }
+  }
+
+  /** Method that creates a new game for the player */
+  private void newGame() {
+    g = new Game();
+    look.setEnabled(true);
+    pickup.setEnabled(true);
+    drop.setEnabled(true);
+    eat.setEnabled(true);
+    list.setEnabled(true);
+    help.setEnabled(true);
+    slide.setEnabled(true);
+    toss.setEnabled(true);
+    north.setEnabled(true);
+    east.setEnabled(true);
+    west.setEnabled(true);
+    south.setEnabled(true);
+    showMinimap.setEnabled(true);
+    g.setWelcomeMessage();
+    results.setText("");
+  }
 }
--- Location.java
+++ Location.java
@@ -1,4 +1,5 @@
 package BORK;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 /**
@@ -7,115 +8,114 @@ import java.util.ArrayList;
  * @author Kevin Smith, Haris Islamcevic
  * @version 02/24/2019
  */
-public class Location
-{
-    /** Instance variable for name */
-    private String description;
+public class Location {
+  /** Instance variable for name */
+  private String description;
 
-    /** Instance variable for item */
-    private Item item;
+  /** Instance variable for item */
+  private Item item;
 
-    /** Instance variable of HashMap of object String and Location */
-    private HashMap <String,Location> myNeighbors = new HashMap<String,Location>();
+  /** Instance variable of HashMap of object String and Location */
+  private HashMap<String, Location> myNeighbors = new HashMap<String, Location>();
 
-    /** Instance variable of an array of items */
-    private Item[] items = new Item[1];
+  /** Instance variable of an array of items */
+  private Item[] items = new Item[1];
 
-    /** Instance variable for current item */
-    private Item currentItem;
+  /** Instance variable for current item */
+  private Item currentItem;
 
-    /** Instance variable of ArrayList of object Item */
-    private ArrayList <Item> mapItems = new ArrayList <Item>();
+  /** Instance variable of ArrayList of object Item */
+  private ArrayList<Item> mapItems = new ArrayList<Item>();
 
-    /**
-     * Constructor for objects of class location
-     */
-    public Location(String pDescription){
-        description = pDescription;
-        item = null;
+  /** Constructor for objects of class location */
+  public Location(String pDescription) {
+    description = pDescription;
+    item = null;
+  }
 
+  /**
+   * Constructor for objects of class location
+   *
+   * @param pDescription, pItem
+   */
+  public Location(String pDescription, Item pItem) {
+    description = pDescription;
+    item = pItem;
+    currentItem = pItem;
+    mapItems.add(currentItem);
+  }
+
+  /**
+   * Method to get the item
+   *
+   * @return currentItem
+   */
+  public Item getItem() {
+    return currentItem;
+  }
+
+  /**
+   * Method to get the description
+   *
+   * @return description
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * Method to add an item to our array
+   *
+   * @param pItem
+   */
+  public void addItem(Item pItem) {
+    item = pItem;
+    currentItem = pItem;
+    mapItems.add(currentItem);
+  }
+
+  /** Method to see if a room has an item */
+  public boolean hasItem() {
+    if (mapItems.size() == 0 || items == null) {
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * Constructor for objects of class location
-     * @param pDescription, pItem
-     */
-    public Location(String pDescription, Item pItem){
-        description = pDescription;
-        item = pItem;
-        currentItem = pItem;
-        mapItems.add(currentItem);
-    }
+  /**
+   * Method to add neighbors around your location
+   *
+   * @param pDir,pLoc
+   */
+  public void addNeighbor(String pDir, Location pLoc) {
+    myNeighbors.put(pDir, pLoc);
+  }
 
-    /**
-     * Method to get the item
-     * @return currentItem
-     */
-    public Item getItem(){
-        return currentItem;
-    }
+  /**
+   * Method to get the neighbor
+   *
+   * @param pDirection
+   */
+  public Location getNeighbor(String pDirection) {
+    return myNeighbors.get(pDirection);
+  }
 
-    /**
-     * Method to get the description
-     * @return description
-     */
-    public String getDescription(){
-        return description;
-    }
+  /**
+   * Method to remove the item from our array
+   *
+   * @return currentItem
+   */
+  public Item removeItem() {
+    items = null;
+    item = null;
+    return currentItem;
+  }
 
-    /**
-     * Method to add an item to our array
-     * @param pItem
-     */
-    public void addItem(Item pItem){
-        item = pItem;
-        currentItem = pItem;
-        mapItems.add(currentItem);
+  /** Method to get the long description */
+  public String getLongDescription() {
+    if (hasItem() == true) {
+      return ("You are in the " + description + " room," + " You see  " + item.getName());
     }
-
-    /**
-     * Method to see if a room has an item
-     */
-    public boolean hasItem() {
-        if(mapItems.size() == 0 || items == null){
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Method to add neighbors around your location
-     * @param pDir,pLoc
-     */
-    public void addNeighbor(String pDir, Location pLoc){
-        myNeighbors.put(pDir,pLoc);
-    }
-
-    /**
-     * Method to get the neighbor
-     * @param pDirection
-     */
-    public Location getNeighbor(String pDirection){
-        return myNeighbors.get(pDirection);
-    }
-
-    /**
-     * Method to remove the item from our array
-     * @return currentItem
-     */
-    public Item removeItem(){
-        items = null;
-        item = null;
-        return currentItem;
-    }
-
-    /**
-     * Method to get the long description
-     */
-    public String getLongDescription(){
-        if(hasItem() == true ){
-            return ("You are in the " + description + " room," + " You see  " + item.getName());
-        }
-        return ("You are in the " + description + " room.");
-    }
+    return ("You are in the " + description + " room.");
+  }
 }
